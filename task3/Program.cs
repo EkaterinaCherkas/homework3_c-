99b4003b0fbc71e1f53f2639b2b979b6ba6e8205
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-03 02:30:31 +0300

    add task3 с условием что N может быть меньше 0

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//* при N < 0, нужно вывести от N до -1
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+int masiv = N;
+if (N < 0){
+    i = N;
+    masiv = -N;
+    N = -1;}
+int[] result = new int[masiv];
+for ( int j = 0; i<=N ; i++){
+    result[j] = i * i * i;
+    j++;
+}
+for (int j = 0; j < masiv; j++){
+    Console.WriteLine($"{result[j]}" );
+}
